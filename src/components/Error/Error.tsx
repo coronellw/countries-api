bc1c8b7bf1eafa0421fe77d87034e9697615d2ca
Wiston <coronell@gmail.com>
2023-10-23 15:48:44 -0300

    adding some style to error page 🐛

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -1,4 +1,5 @@
 import { useRouteError } from "react-router-dom";
+import './Error.scss'
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -9,7 +10,9 @@ export default function ErrorPage() {
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{JSON.stringify(error)}</i>
+        <code>
+          <i>{JSON.stringify(error)}</i>
+        </code>
       </p>
     </div>
   );
